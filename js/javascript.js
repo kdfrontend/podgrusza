5299834d5e7231ff20cc3c49416de0f90ed6ac00
kdfrontend <35347535+kdfrontend@users.noreply.github.com>
2018-04-10 16:25:37 +0200

    Fundacja pod gruszą 10.04.18
    wersja 1

---
+++ js/javascript.js
@@ -0,0 +1,25 @@
+$(".menu-hamburger").on('click', function () {
+    $(this).toggleClass("on");
+    $('.menu-section').toggleClass("on");
+    $("nav ul").toggleClass('nonactive');
+    $("nav div").toggleClass('hidden');
+    $('.logo').toggleClass('hidden');
+});
+
+
+$('.left-facebook').hover(function () {
+    $(".facebook-timeline").removeClass("hidden");
+
+});
+$(".facebook-timeline").mouseout(function () {
+    $(this).toggleClass("hidden");
+
+})
+$('.right-twitter').hover(function () {
+    $(".twitter-timeline").removeClass("hidden");
+
+});
+$(".twitter-timeline").mouseout(function () {
+    $(this).toggleClass("hidden");
+
+})
